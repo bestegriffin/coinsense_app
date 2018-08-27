98bea3bb43c67ace46f5d5fd8298abfc2598c3cc
bestegriffin <griffinbeste@gmail.com>
2018-08-27 15:43:37 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 3000
 var app = express();
 mongoose.Promise = require('bluebird');
 
 // use sessions for tracking logins
 app.use(session({
-  secret: 'Crypto loves you',
-  resave: true,
-  saveUninitialized: false
+    secret: 'Crypto loves you',
+    resave: true,
+    saveUninitialized: false
 }));
 
 // make user ID available in templates
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.session.userId;
-  next();
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.session.userId;
+    next();
 });
 
 // mongodb connection
-mongoose.connect("mongodb://heroku_crtwx80t:cujsjdqt01oar8r80vkl2p2024@ds163836.mlab.com:63836/heroku_crtwx80t", {useMongoClient: true});
+// development db
+// mongoose.connect("mongodb://localhost:27017/coinSense", { useMongoClient: true });
+
+// // deployment db
+mongoose.connect("mongodb://heroku_skn5zmbj:e9PoDTXwzlJoulQZXn2Rx-fSCTRp5mkJ@ds251277.mlab.com:51277/heroku_q941fpr3");
+
 var db = mongoose.connection;
 // mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -42,24 +47,24 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 // define as the last app.use callback
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
 
 // listen on port 3000
-app.listen(port, function () {
-  console.log('Express app listening on port 8000');
+app.listen(port, function() {
+    console.log('Express app listening on port  ' + port);
 });
